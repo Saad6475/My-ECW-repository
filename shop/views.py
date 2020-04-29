56626ff1026808d701565bf78735a4c1cb8958fe
saad <saad.ali6475@gmail.com>
2020-04-29 21:54:05 +0500

    add search commit

--- shop/views.py
+++ shop/views.py
@@ -23,6 +23,33 @@ def index(request):
     perms = {'allprods' : allprods}
     return render(request,'shop/index.html', perms)
 
+
+def searchMatch(query, item):
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query= request.GET.get('search')
+
+    allprods = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nslides = n // 4 + ceil((n / 4) - (n // 4))
+
+        if len(prod) != 0 :
+            allprods.append([prod, range(1, nslides), nslides])
+
+    perms = {'allprods': allprods}
+    return render(request, 'shop/search.html', perms)
+
 def about(request):
     return render(request,'shop/about.html')
 
@@ -85,9 +112,6 @@ def tracker(request):
 
     return render(request, 'shop/tracker.html')
 
-
-def search(request):
-    return HttpResponse ("search")
 
 
 def products(request, myid):
